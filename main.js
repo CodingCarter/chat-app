7ef37f1458c6e41159ca22f9af3e506291e0a9bb
Carter Snook <cartersnook04@gmail.com>
2019-10-06 13:48:39 -0500

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const form = document.querySelector("form");
 const input = document.querySelector(".input-message");
 const messages = document.querySelector(".messages");
 var username = document.querySelector('.input-username').value;//prompt("Please enter a nickname: ", "")
-const socket = io('http://104.248.120.187/');
+const socket = io('/');
 
 function randomNumbers(max, len) { var str = ''; for (var i = len-1; i >= 0; i--) { str += Math.floor(Math.random() * Math.floor(max)); } return str; }
 
